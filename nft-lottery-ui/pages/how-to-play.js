fc89dc09e215fa75fd5d37b24884649012306b06
leo <leonard.ll.l81@gmail.com>
2022-05-27 23:53:06 +0200

    🎨

--- nft-lottery-ui/pages/how-to-play.js
+++ nft-lottery-ui/pages/how-to-play.js
@@ -1,8 +1,13 @@
+import howtoplay from "../public/raffle_house.svg"
+import Image from "next/image"
 const HowToPlay = (params) => {
     return (
         <div className="section">
             <div className="container">
                 <h1 className="text-white">How To Play</h1>
+            </div>
+            <div className="container how-to-play h-100" style={{backgroundImage:"../public/raffle_house.svg", vheight:"100", width:"100%"}}>
+                 
             </div>
         </div>
     )
