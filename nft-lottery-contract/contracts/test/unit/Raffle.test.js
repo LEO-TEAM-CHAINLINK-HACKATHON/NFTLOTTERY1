09936803d2d6880870647b1aeabadc40f8d27534
leo <lonardlator@yahoo.fr>
2022-09-29 21:05:46 +0200

    🐛 zero test pass fix attempt

--- nft-lottery-contract/contracts/test/unit/Raffle.test.js
+++ nft-lottery-contract/contracts/test/unit/Raffle.test.js
@@ -6,21 +6,21 @@ const { developementChains, networkConfig } = require("/../..helper-hardhat-conf
 !developementChains.includes(network.name)
     ? describe.skip
     : describe("Raffle Unit Tests", async function () {
-          let raffle, vrfCoordinatorV2Mock, interval
+          let raffle, vrfCoordinatorV2Mock, interval, deployer
           const chainId = network.config.chainId
 
           beforeEach(async function () {
-              const { deployer } = await getNamedAccounts()
+              deployer = (await getNamedAccounts()).deployer
               await deployments.fixture(["all"])
               raffle = await ethers.getContract("Raffle", deployer)
               vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+              interval = await raffle.getInterval()
           })
           describe("constructor", function () {
               it("initialize the raffle correctly", async function () {
                   // Ideally 1 assert per it
                   const raffleState = (await raffle.getRaffleState()).toString()
                   assert.equal(raffleState, "0")
-                  interval = await raffle.getInterval()
                   assert.equal(interval.toString(), networkConfig[chainId]["keepersUpdateInterval"])
               })
           })
