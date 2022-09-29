3f8b23d91967891493a5d714756618ca7e89e3af
M.Siddique <108999811+ABossOfMyself@users.noreply.github.com>
2022-09-30 00:59:13 +0500

    Update Raffle.test.js

--- nft-lottery-contract/contracts/test/unit/Raffle.test.js
+++ nft-lottery-contract/contracts/test/unit/Raffle.test.js
@@ -1,7 +1,7 @@
 const { assert } = require("chai")
 const { getNamedAccounts, deployments, ethers, network } = require("hardhat")
 
-const { developmentChains, networkConfig } = require("/../..helper-hardhat-config")
+const { developmentChains, networkConfig } = require("../..helper-hardhat-config")
 
 !developmentChains.includes(network.name)
     ? describe.skip
