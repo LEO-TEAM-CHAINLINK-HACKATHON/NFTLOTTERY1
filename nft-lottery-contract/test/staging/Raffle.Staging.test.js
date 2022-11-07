029acf975a8d789836c229bbe983ff9ab9fb5bb1
Leonardll <lonardlator@yahoo.fr>
2022-11-07 10:07:47 +0100

    🐛 typo correction - staging test passing attempt

--- nft-lottery-contract/test/staging/Raffle.Staging.test.js
+++ nft-lottery-contract/test/staging/Raffle.Staging.test.js
@@ -5,7 +5,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
 
 !developmentChains.includes(network.name)
     ? describe.skip
-    : describe("Raffle Unit Tests", function () {
+    : describe("Raffle Staging Tests", function () {
           let raffle, vrfCoordinatorV2Mock, interval, deployer, raffleEntranceFee
           const chainId = network.config.chainId
 
@@ -13,27 +13,29 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
               deployer = (await getNamedAccounts()).deployer
               await deployments.fixture(["all"])
               raffle = await ethers.getContract("Raffle", deployer)
-              vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
-              interval = await raffle.getInterval()
+              //   vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+              //   interval = await raffle.getInterval()
               raffleEntranceFee = await raffle.getEntranceFee()
           })
 
           describe("fulfillRandomWords", function () {
               it("works with live chainlink keepers and  chainlink vrf, got random winner", async function () {
-                  const startingTimeStamp = await raffle.getStartingTimeStamp()
-                  const accounts = await ethers.getSigners
+                  console.log("setting up test")
+                  const startingTimeStamp = await raffle.getLastTimeStamp()
+                  const accounts = await ethers.getSigners()
+                  console.log("Setting up listener...")
 
                   await new Promise(async (resolve, reject) => {
                       // set up listener before user enter raffle
                       //just in case blockchain runs fast
                       raffle.once("WinnerPicked", async () => {
-                          console.log("winner picked")
+                          console.log("winner picked event fired")
                           resolve()
                           try {
                               const recentWinner = await raffle.getRecentWinner()
                               const raffleState = await raffle.getRaffleState()
                               const winnerEndingBalance = await accounts[0].getBalance()
-                              const endingBalance = await raffle.getLastTimeStamp()
+                              const endingTimeStamp = await raffle.getLastTimeStamp()
                               await expect(raffle.getPlayers(0)).to.be.reverted
                               assert.equal(recentWinner.toSring(), accounts[0].address)
                               assert.equal(raffleState, 0)
@@ -48,7 +50,11 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                           }
                       })
                       // then enter raffle
-                      await raffle.enterRaffle({ value: raffleEntranceFee })
+                      console.log("entering raffle...")
+
+                      const tx = await raffle.enterRaffle({ value: raffleEntranceFee })
+                      await tx.wait(1)
+                      console.log("Ok, time to wait...")
                       const winnerStartingBalance = await accounts[0].getBalance()
 
                       // all code below will only complete when listener has finised its job
